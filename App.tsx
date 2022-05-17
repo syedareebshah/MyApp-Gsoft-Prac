10b6e844ac75694ef72b085c99b90a328724d005
Syed Areeb Shah <64960394+syedareebshah@users.noreply.github.com>
2022-05-17 18:05:27 +0500

    some basic redux setup

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Navigation from './src/config/Navigation';
-import { Provider as PaperProvider} from 'react-native-paper';
+import { Provider as PaperProvider } from 'react-native-paper';
 import { theme } from './src/config/theme-paper';
 
 const App = () => {
 
   return (
-    <PaperProvider theme={theme}>
-    <Navigation />
-    </PaperProvider>
+      <PaperProvider theme={theme}>
+        <Navigation />
+      </PaperProvider>
   )
 };
 
